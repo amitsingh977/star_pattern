226e670ff28d45b70d033c43fce02204c78ee833
TheCreator-VA <47741137+TheCreator-VA@users.noreply.github.com>
2020-01-07 16:51:12 +0530

    Creating a simple pyramid in c

--- pyramid.c
+++ pyramid.c
@@ -2,7 +2,7 @@
 int main()
 {
 	int i,j,k,n;
-	scanf("%d",&n);//Enter the number of lines in pyramid
+	scanf("%d",&n);//Enter the number of lines in pyramid 
 	for(i=1;i<=n;i++)
 	{
 		for(j=n;j>i;j--)
